a37f13dffcdfe17d61db1f796645c4d08e06526b
Denis112500 <denissanta401@gmail.com>
2026-05-25 13:59:35 +0300

    feature: agent loop with conversation history (short term memory)

--- helios.py
+++ helios.py
@@ -1,13 +1,21 @@
 import os
 from litellm import completion
 
-response = completion(
-    model="openai/gpt-4.1",
-    api_base="https://models.inference.ai.azure.com",
-    api_key=os.environ["GITHUB_TOKEN"],
-    messages=[
-        {"role": "system", "content": "Say hello and tell me what model you are."},
-    ]
-)
+user_messages=[
+{"role": "system", "content": "You are Helios, a helpful assistant."},
+]
 
-print (response.choices[0].message.content)
+while True:
+    user_input = input ("You: ")
+    user_messages.append({"role": "user", "content": user_input})
+
+    response = completion(
+        model="openai/gpt-4.1",
+        api_base="https://models.inference.ai.azure.com",
+        api_key=os.environ["GITHUB_TOKEN"],
+        messages=user_messages
+    )
+
+    reply = response.choices[0].message.content
+    user_messages.append({"role": "assistant", "content": reply})
+    print ("Helios: " + reply)
